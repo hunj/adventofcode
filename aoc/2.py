d300845ec4f7f730ac826a6fba3bd13382dd6ec2
Hun Jae Lee <hunjaelee@protonmail.com>
2023-12-11 11:35:33 -0500

    add 2

---
+++ aoc/2.py
@@ -0,0 +1,42 @@
+from utils import aoc
+from functools import reduce
+import re
+
+data = aoc.get_data_for(day=2)
+
+ids = []
+record = {}
+
+# part 1
+
+for line in data[:-1]:
+
+    current = line[5:]
+    re_match = re.match(r'^(\d+): (.*)$', current)
+
+    game_id = int(re_match[1])
+    for match in re_match[2].split(';'):
+        for pair in match.split(','):
+            pair = pair.strip()
+            pair_re_match = re.match(r'(\d+) (\w+)', pair)
+            count, color = int(pair_re_match[1]), pair_re_match[2]
+            if not record.get(game_id):
+                record[game_id] = {}
+            if not record[game_id].get(color):
+                record[game_id][color] = 0
+            if record[game_id][color] < count:
+                record[game_id][color] = count
+
+for game_id, values in record.items():
+    if values['red'] <= 12 and values['green'] <= 13 and values['blue'] <= 14:
+        ids.append(game_id)
+
+print(sum(ids))
+
+# part 2
+
+powers = []
+for cubes in record.values():
+    powers.append(reduce(lambda x, y: x * y, cubes.values()))
+
+print(sum(powers))
