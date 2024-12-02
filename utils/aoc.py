1f6aa14eebe7507d0f195ff5e26a25704c49378c
hunj <hunjaelee@protonmail.com>
2024-12-02 02:07:22 -0500

    update

--- utils/aoc.py
+++ utils/aoc.py
@@ -8,11 +8,11 @@ load_dotenv(dotenv_path=Path('.env'))
 AOC_SESSION = environ['AOC_SESSION']
 
 
-def get_data_for(day: int = 0) -> List[str]:
+def get_data_for(year: int = 2024, day: int = 0) -> List[str]:
     if not day:
         raise Exception()
 
-    url = f"https://adventofcode.com/2023/day/{day}/input"
+    url = f"https://adventofcode.com/{year}/day/{day}/input"
 
     res = requests.get(url, cookies={'session': AOC_SESSION})
-    return res.content.decode().split('\n')
+    return res.content.decode().strip().split('\n')
